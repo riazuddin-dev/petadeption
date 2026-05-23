3466124a0c0cb2f31577a2cf9a2946ec1e4b712b
riazut <riazuddin7209@gmail.com>
2026-05-23 18:54:09 +0600

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,48 +1,109 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono
+
+} from "next/font/google";
+
 import "./globals.css";
 
+import {
+  Toaster
+} from "sonner";
 
-import { Toaster }
-from "sonner";
+const geistSans =
+Geist({
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable:
+  "--font-geist-sans",
+
+  subsets:
+  ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const geistMono =
+Geist_Mono({
+
+  variable:
+  "--font-geist-mono",
+
+  subsets:
+  ["latin"],
 });
 
 export const metadata = {
-  title: "PawsNest",
-  description: "Pet Adoption Platform",
+
+  title:
+  "PawHaven | Pet Adoption Platform",
+
+  description:
+  "Find loving pets and give them a forever home with PawHaven.",
+
+  keywords: [
+
+    "Pet Adoption",
+
+    "Dogs",
+
+    "Cats",
+
+    "PawHaven",
+
+    "Animal Shelter",
+
+    "Adopt Pets",
+  ],
+
+  authors: [
+
+    {
+      name:
+      "PawHaven",
+    },
+  ],
+
+  icons: {
+
+    icon:
+    "/logo.png",
+  },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}) {
+
   return (
+
     <html
+
       lang="en"
+
       data-theme="light"
+
       className={`
+
         ${geistSans.variable}
+
         ${geistMono.variable}
+
         h-full
+
         scroll-smooth
+
         bg-[#F4EFE6]
       `}
     >
-     <body>
 
-  {children}
+      <body>
 
-  <Toaster
-    richColors
-    position="top-right"
-  />
+        {children}
 
-</body>
+        <Toaster
+          richColors
+          position="top-right"
+        />
+
+      </body>
     </html>
   );
 }
